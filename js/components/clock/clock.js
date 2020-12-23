9a01e4a0f2607d0bfbf00cc78f90b75f15f76772
Justas383 <74533002+Justas383@users.noreply.github.com>
2020-12-23 10:03:13 +0200

    initial clock

--- js/components/clock/clock.js
+++ js/components/clock/clock.js
@@ -1,5 +1,24 @@
-class clock {
-    constructor( )
+class Clock {
+    constructor( ){
+        this.now = Date.now();
+            this.deadline = {
+                year: (new Date()).getFullYear(),
+                month: 11,
+                day: 16,
+                hour: 10,
+                minutes: 0,
+                seconds: 0
+
+                
+            }
+
+            this.dateString = this.createDateString();
+        console.log('konstruojam laikrodi..')
+    }
+    createDateString(){
+        const { year, month, day, hour, minutes, seconds} = this.deadline 
+        return`${year}-${month}-${day} ${hour}:${minutes}:${seconds}`;
+    }
 }
 
-export {clock}
+export {Clock}
--- js/main.js
+++ js/main.js
@@ -4,10 +4,17 @@ import {renderProgressBar} from './components/progress-bar/renderProgressBar.js'
 
 import {socialsData} from './components/socials/socialsData.js'
 import {renderSocials} from './components/socials/renderSocials.js'
+import {Clock} from './components/clock/clock.js'
+
+
+const clock = new Clock
+console.log(clock)
 
 renderProgressBar('.left', progressBarData);
 
 renderSocials('footer > .row-long', socialsData);
+
+renderClock('.row-short > .clock', clockData)
 
 console.log('js is running here');
 
